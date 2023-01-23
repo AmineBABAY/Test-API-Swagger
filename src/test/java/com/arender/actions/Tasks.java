f0a876ceb74d9adc41e448850231917205c39bb0
Ilyes FATFOUTI <ilyes.fatfouti@arondor.com>
2023-01-23 17:31:08 +0100

    add perform test

---
+++ src/test/java/com/arender/actions/Tasks.java
@@ -0,0 +1,71 @@
+package com.arender.actions;
+
+import java.util.ArrayList;
+
+import com.arender.endpoint.Documents;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+public class Tasks extends AssertActions {
+    private ArrayList<Response> tabResponses;
+    private String name;
+    private int numberOfSuccessRequest=0;
+
+    public Tasks(String file)
+    {  
+        tabResponses= new ArrayList<Response>();
+        Thread.currentThread().setName("Im the user : " +Thread.currentThread().getName());
+        this.name=Thread.currentThread().getName();
+        for(int i=1;i<=2;i++)
+        {
+        Response upload=Documents.uploadDocument(file);
+        tabResponses.add(upload);
+        if(upload.getStatusCode()==200)
+        {
+            this.numberOfSuccessRequest++;
+        }
+        Response getLayout=Documents.getDocumentLayout(JsonPath.from(upload.asString()).get("id"));
+        if(getLayout.getStatusCode()==200)
+        {
+            this.numberOfSuccessRequest++;
+        }
+        tabResponses.add(getLayout);
+        Response getImage = Documents.getPageImage(JsonPath.from(upload.asString()).get("id"), 0);
+        if(getImage.getStatusCode()==200)
+        {
+            this.numberOfSuccessRequest++;
+        }
+        tabResponses.add(getImage);
+
+        }
+        
+        
+        
+    }
+
+    public int getNumberOfSuccessRequest()
+    {
+        return numberOfSuccessRequest;
+    }
+
+    public void setNumberOfSuccessRequest(int numberOfSuccessRequest)
+    {
+        this.numberOfSuccessRequest = numberOfSuccessRequest;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public ArrayList<Response> getTabResponses()
+    {
+        return tabResponses;
+    }    
+}
--- src/test/java/com/arender/tests/PerformanceTest.java
+++ src/test/java/com/arender/tests/PerformanceTest.java
@@ -1,39 +1,68 @@
 package com.arender.tests;
 
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.testng.annotations.Test;
-
 import com.arender.actions.AssertActions;
-import com.arender.endpoint.Documents;
-
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
+import com.arender.actions.Tasks;
 
 public class PerformanceTest extends AssertActions
 {
-    
-    String documentId="";
-    
-    
-    @Test(priority =1)
-    public void  uploadDocument() throws InterruptedException
-    {
-        ExecutorService executor = Executors.newFixedThreadPool(numberOfUsers);
 
-        // Submit 10 tasks to the ExecutorService
-        for (int i = 0; i < 100; i++) {
-            executor.submit(() -> {
-                Response response= Documents.uploadDocument(file);
-                verifyStatusCode(response, 200);
+    String documentId = "";
+
+    @Test(priority = 1)
+    public static void uploadDocument() throws InterruptedException
+    {
+        ArrayList<Tasks> tabTasks = new ArrayList<Tasks>();
+        ExecutorService executorPDF = Executors.newFixedThreadPool(numberOfUsers);
+        ExecutorService executorTXT = Executors.newFixedThreadPool(numberOfUsers);
+        ExecutorService executorDOCX = Executors.newFixedThreadPool(numberOfUsers);
+        AtomicInteger completed = new AtomicInteger();
+        for (int i = 1; i <= numberOfUsers; i++)
+        {
+
+            executorPDF.submit(() -> {
+                tabTasks.add(new Tasks("pdf"));
+                completed.incrementAndGet();
+
+            });
+            executorTXT.submit(() -> {
+                tabTasks.add(new Tasks("txt"));
+                completed.incrementAndGet();
+            });
+            executorDOCX.submit(() -> {
+                tabTasks.add(new Tasks("docx"));
+                completed.incrementAndGet();
+
             });
         }
 
-        // Shut down the ExecutorService and wait for all tasks to complete
-        executor.shutdown();
-        executor.awaitTermination(10, TimeUnit.MINUTES);
+        executorPDF.shutdown();
+        executorPDF.awaitTermination(10, TimeUnit.MINUTES);
+        executorTXT.shutdown();
+        executorTXT.awaitTermination(10, TimeUnit.MINUTES);
+        executorDOCX.shutdown();
+        executorDOCX.awaitTermination(10, TimeUnit.MINUTES);
+        //System.out.println(completed.get());
+        for (int i = 0; i < tabTasks.size(); i++)
+        {
+            Tasks task=tabTasks.get(i);
+            System.out.println("\t user name : " + task.getName());
+            System.out.println("\t number of succes request is  : " + task.getNumberOfSuccessRequest());
+            for (int j = 0; j < task.getTabResponses().size(); j++)
+            {
+                
+            System.out.print("\t code status of response  : " + (j+1) + " "+ task.getTabResponses().get(j).getStatusCode());
+            System.out.print("\t time of response "+ (j+1)  +" : "+ task.getTabResponses().get(j).time());
+            System.out.println(" ");
+        }
 
     }
+        System.out.println("Total number of users : " + completed.get());
+}
 }
