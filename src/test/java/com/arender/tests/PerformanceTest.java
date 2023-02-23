c1c3b78779c5f24cdf3b656f1a0aa98f6337a42c
Ilyes FATFOUTI <ilyes.fatfouti@arondor.com>
2023-02-23 09:32:01 +0100

    QA-2756 calculate max,min and average then generate a graph

--- src/test/java/com/arender/tests/PerformanceTest.java
+++ src/test/java/com/arender/tests/PerformanceTest.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -17,6 +18,7 @@ import org.knowm.xchart.BitmapEncoder;
 import org.knowm.xchart.BitmapEncoder.BitmapFormat;
 import org.knowm.xchart.CategoryChart;
 import org.knowm.xchart.CategoryChartBuilder;
+import org.knowm.xchart.style.Styler.ChartTheme;
 import org.knowm.xchart.style.Styler.LegendPosition;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
@@ -37,7 +39,9 @@ public class PerformanceTest extends AssertActions
     private static CategoryChart chart;
 
     long totalUpload = 0, totalLayout = 0, totalGetImage100px = 0, totalGetImage800px = 0, totalEvic = 0,
-            numberOfGetLayout100px = 0, numberOfGetLayout800px = 0;
+            numberOfGetLayout100px = 0, numberOfGetLayout800px = 0, maxOfUpload = 0, maxOfLayout = 0,
+            maxOfGetImage100px = 0, maxOfGetImage800px = 0, maxOfEvic = 0, minOfUpload = 0, minOfLayout = 0,
+            minOfGetImage100px = 0, minOfGetImage800px = 0, minOfEvic = 0;
 
     @BeforeSuite
     public static void initialization()
@@ -48,20 +52,19 @@ public class PerformanceTest extends AssertActions
 
     }
 
+    // public long findMax()
+
     public void testMultipleRequests(File fileToUploadGP1, File fileToUploadGP2, File fileToUploadGP3)
             throws InterruptedException, IOException
     {
         ArrayList<Tasks> tabTasks = new ArrayList<Tasks>();
 
-        LOGGER.info("Test has been started.");
-        LOGGER.info("Available processors : " + Runtime.getRuntime().availableProcessors());
-        ExecutorService executorGroup1 = Executors.newFixedThreadPool(numberOfUsers);
-        ExecutorService executorGroup2 = Executors.newFixedThreadPool(numberOfUsers);
-        ExecutorService executorGroup3 = Executors.newFixedThreadPool(numberOfUsers);
+        ExecutorService executorGroup = Executors.newFixedThreadPool(numberOfUsers);
+
         AtomicInteger completed = new AtomicInteger();
         for (int i = 1; i <= numberOfUsers; i++)
         {
-            executorGroup1.submit(() -> {
+            executorGroup.submit(() -> {
                 try
                 {
                     tabTasks.add(new Tasks(fileToUploadGP1));
@@ -72,44 +75,52 @@ public class PerformanceTest extends AssertActions
                     LOGGER.error(e.getMessage());
                 }
             });
-            executorGroup2.submit(() -> {
-                try
-                {
-                    tabTasks.add(new Tasks(fileToUploadGP2));
-                    completed.incrementAndGet();
-                }
-                catch (Exception e)
-                {
-                    LOGGER.error(e.getMessage());
-                    ;
-                }
 
-            });
-            executorGroup3.submit(() -> {
-                try
-                {
-                    tabTasks.add(new Tasks(fileToUploadGP3));
-                    completed.incrementAndGet();
-                }
-                catch (Exception e)
-                {
-                    LOGGER.error(e.getMessage());
-                }
-
-            });
         }
-        executorGroup1.shutdown();
-        executorGroup1.awaitTermination(3, TimeUnit.MINUTES);
-        executorGroup2.shutdown();
-        executorGroup2.awaitTermination(3, TimeUnit.MINUTES);
-        executorGroup3.shutdown();
-        executorGroup3.awaitTermination(3, TimeUnit.MINUTES);
-
+        executorGroup.shutdown();
+        executorGroup.awaitTermination(3, TimeUnit.MINUTES);
         for (int i = 0; i < tabTasks.size(); i++)
         {
             Tasks task = tabTasks.get(i);
             tabAllTasks.add(task);
-            LOGGER.info("\n Im the user " + task.getName() + "\n");
+            if (maxOfUpload < task.getUploadResponse().time())
+            {
+                maxOfUpload = task.getUploadResponse().time();
+            }
+            if (maxOfLayout < task.getGetLayoutResponse().time())
+            {
+                maxOfLayout = task.getGetLayoutResponse().time();
+            }
+            if (maxOfEvic < task.getEvicResponses().time())
+            {
+                maxOfEvic = task.getGetLayoutResponse().time();
+            }
+
+            if (minOfUpload == 0)
+            {
+                minOfUpload = tabTasks.get(0).getUploadResponse().time();
+            }
+            else if (minOfUpload > task.getUploadResponse().time())
+            {
+                minOfUpload = task.getUploadResponse().time();
+            }
+
+            if (minOfLayout == 0)
+            {
+                minOfLayout = tabTasks.get(0).getGetLayoutResponse().time();
+            }
+            else if (minOfLayout > task.getGetLayoutResponse().time())
+            {
+                minOfLayout = task.getGetLayoutResponse().time();
+            }
+            if (minOfEvic == 0)
+            {
+                minOfEvic = tabTasks.get(0).getEvicResponses().time();
+            }
+            else if (minOfEvic > task.getEvicResponses().time())
+            {
+                minOfEvic = task.getEvicResponses().time();
+            }
             totalUpload += task.getUploadResponse().time();
 
             totalLayout += task.getGetLayoutResponse().time();
@@ -119,10 +130,34 @@ public class PerformanceTest extends AssertActions
             for (int l = 0; l < task.getTabGetImage100pxResponses().size(); l++)
             {
                 totalGetImage100px += task.getTabGetImage100pxResponses().get(l).time();
+                if (maxOfGetImage100px < task.getTabGetImage100pxResponses().get(l).time())
+                {
+                    maxOfGetImage100px = task.getTabGetImage100pxResponses().get(l).time();
+                }
+                if (minOfGetImage100px == 0)
+                {
+                    minOfGetImage100px = task.getTabGetImage100pxResponses().get(0).time();
+                }
+                else if (minOfGetImage100px > task.getTabGetImage100pxResponses().get(l).time())
+                {
+                    minOfGetImage100px = task.getTabGetImage100pxResponses().get(l).time();
+                }
             }
             for (int m = 0; m < task.getTabGetImage800pxResponses().size(); m++)
             {
                 totalGetImage800px += task.getTabGetImage800pxResponses().get(m).time();
+                if (maxOfGetImage800px < task.getTabGetImage800pxResponses().get(m).time())
+                {
+                    maxOfGetImage800px = task.getTabGetImage800pxResponses().get(m).time();
+                }
+                if (minOfGetImage800px == 0)
+                {
+                    minOfGetImage800px = task.getTabGetImage800pxResponses().get(0).time();
+                }
+                else if (minOfGetImage800px > task.getTabGetImage800pxResponses().get(m).time())
+                {
+                    minOfGetImage800px = task.getTabGetImage800pxResponses().get(m).time();
+                }
             }
 
             totalEvic += task.getEvicResponses().time();
@@ -135,6 +170,8 @@ public class PerformanceTest extends AssertActions
     @Test()
     public void PerforamnceTestInShortDuration() throws InterruptedException, IOException
     {
+        LOGGER.info("Test has been started.");
+        LOGGER.info("Available processors : " + Runtime.getRuntime().availableProcessors());
         Instant start = Instant.now();
         Duration duration = Duration.ofMinutes(2);
         while (Duration.between(start, Instant.now()).compareTo(duration) < 0)
@@ -142,39 +179,48 @@ public class PerformanceTest extends AssertActions
             testMultipleRequests(fileToUploadFromConfig, fileToUploadFromConfig, fileToUploadFromConfig);
 
         }
-        chart = new CategoryChartBuilder().width(1920).height(1080).title("Graph of requests")
-                .xAxisTitle("Name of request").yAxisTitle("Time").build();
-        chart.getStyler().setLegendPosition(LegendPosition.InsideNW);
+        chart = new CategoryChartBuilder().title("Average of requests for : " + file).xAxisTitle("Request")
+                .yAxisTitle("Time in milliseonds").theme(ChartTheme.Matlab).build();
+        chart.getStyler().setLegendPosition(LegendPosition.OutsideE);
+        chart.getStyler().setLabelsVisible(true);
+        // chart.getStyler().setPlotGridLinesVisible(true);
         ArrayList<Long> averageOfUpload = new ArrayList<Long>();
         ArrayList<Long> averageOfLayout = new ArrayList<Long>();
         ArrayList<Long> averageOfGetImage100px = new ArrayList<Long>();
         ArrayList<Long> averageOfGetImage800px = new ArrayList<Long>();
         ArrayList<Long> averageOfEvic = new ArrayList<Long>();
+        ArrayList<String> nameOfAxis = new ArrayList<String>(Arrays.asList(new String[] { "Average", "Max", "Min" }));
 
         averageOfUpload.add(totalUpload / tabAllTasks.size());
+        averageOfUpload.add(maxOfUpload);
+        averageOfUpload.add(minOfUpload);
+
         averageOfLayout.add(totalLayout / tabAllTasks.size());
+        averageOfLayout.add(maxOfLayout);
+        averageOfLayout.add(minOfLayout);
+
         averageOfGetImage100px.add(totalGetImage100px / numberOfGetLayout100px);
+        averageOfGetImage100px.add(maxOfGetImage100px);
+        averageOfGetImage100px.add(minOfGetImage100px);
+
         averageOfGetImage800px.add(totalGetImage800px / numberOfGetLayout800px);
+        averageOfGetImage800px.add(maxOfGetImage800px);
+        averageOfGetImage800px.add(minOfGetImage800px);
+
         averageOfEvic.add(totalEvic / tabAllTasks.size());
-        ArrayList<String> uploadRequests = new ArrayList<String>();
-        uploadRequests.add("upload");
-        ArrayList<String> layoutRequests = new ArrayList<String>();
-        layoutRequests.add("get layout");
-        ArrayList<String> getImage100pxRequests = new ArrayList<String>();
-        getImage100pxRequests.add("get image 100px");
-        ArrayList<String> getImage800pxRequests = new ArrayList<String>();
-        getImage800pxRequests.add("get image 800px");
-        ArrayList<String> evicRequests = new ArrayList<String>();
-        evicRequests.add("evic");
-        chart.addSeries("upload", uploadRequests, averageOfUpload);
-        chart.addSeries("get layout ", layoutRequests, averageOfLayout);
-        chart.addSeries("get image 100px ", getImage100pxRequests, averageOfGetImage100px);
-        chart.addSeries("get image 800px ", getImage800pxRequests, averageOfGetImage800px);
-        chart.addSeries("evic ", evicRequests, averageOfEvic);
+        averageOfEvic.add(maxOfEvic);
+        averageOfEvic.add(minOfEvic);
+
+        chart.addSeries("upload", nameOfAxis, averageOfUpload);
+        chart.addSeries("get layout ", nameOfAxis, averageOfLayout);
+        chart.addSeries("get image 100px ", nameOfAxis, averageOfGetImage100px);
+        chart.addSeries("get image 800px ", nameOfAxis, averageOfGetImage800px);
+        chart.addSeries("evic ", nameOfAxis, averageOfEvic);
+
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         BitmapEncoder.saveBitmap(chart, outputStream, BitmapFormat.PNG);
         byte[] imageBytes = outputStream.toByteArray();
-        Allure.addAttachment("Graph of requests", "image/png", new ByteArrayInputStream(imageBytes), ".png");
+        Allure.addAttachment("Average of requests", "image/png", new ByteArrayInputStream(imageBytes), ".png");
         LOGGER.info("average of upload  : " + averageOfUpload.get(0));
         LOGGER.info("average of get layout  : " + averageOfLayout.get(0));
         LOGGER.info("average of get image 100px  : " + averageOfGetImage100px.get(0));
