f46f3c9bb1ec398746a0688cb4973b976bdd0257
sarra.manai <sarra.manai@arondor.com>
2023-10-02 15:15:16 +0200

    QA-3056 adapt the transformDocumentWithFDFAnnotations method with the new json

--- src/test/java/com/arender/tests/TransformationTest.java
+++ src/test/java/com/arender/tests/TransformationTest.java
@@ -285,11 +285,16 @@ public class TransformationTest extends AssertActions
         String body;
         // read the json file for transformation
         JSONObject contentFile = Config.readJsonFile("transformationWithFDFAnnotation");
-        // get the transformationdetails object
-        JSONObject transformationdetails = contentFile.getJSONObject("transformationDetails");
+        // get the transformationdetails array
+        JSONArray transformationDetailsArray = contentFile.getJSONArray("transformationDetails");
         // from the transformationdetails object, get the transformationElements
         // array
-        JSONArray transformationElements = transformationdetails.getJSONArray("transformationElements");
+        // JSONArray transformationElements =
+        // transformationdetails.getJSONArray("transformationElements");
+        // get the first transformationDetails object in the array
+        JSONObject firstTransformationDetails = transformationDetailsArray.getJSONObject(0);
+        // get the transformationElements array
+        JSONArray transformationElements = firstTransformationDetails.getJSONArray("transformationElements");
 
         // get the first JSON Object in the JSON transformationElements Array
         JSONObject firstItemObject = (JSONObject) transformationElements.get(0);
